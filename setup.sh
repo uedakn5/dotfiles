e019e9abb7e2ca75c5bdf8ff08b7357d773bc5a9
uedakn5 <ueda.kengo@accelight.co.jp>
2020-12-15 11:24:19 +0900

    諸諸修正

--- setup.sh
+++ setup.sh
@@ -3,10 +3,12 @@
 DOT_FILES=(bashrc bash_profile screenrc nvimrc)
 NVIM_DIR="${HOME}/.config/nvim"
 VIM_DIR="${HOME}/.vim"
+DOTFILENAME="dotfiles"
+DOTFILEPATH="${HOME}/${DOTFILENAME}"
 
 for file in ${DOT_FILES[@]}
 do
-  ln -is $HOME/dotf/$file $HOME/.$file
+  ln -is $DOTFILEPATH/$file $HOME/.$file
 done
 
 # mkdir -p $HOME/.config/fish
@@ -15,15 +17,16 @@ done
 
 mkdir -p $NVIM_DIR
 mkdir -p $VIM_DIR
-ln -is $HOME/dotf/nvimrc ${NVIM_DIR}/init.vim
+ln -is $DOTFILEPATH/nvimrc ${NVIM_DIR}/init.vim
 if [ ! -d ${NVIM_DIR}/rc ]; then
-  ln -s $HOME/dotf/rc ${NVIM_DIR}/rc
+  ln -s $DOTFILEPATH/rc ${NVIM_DIR}/rc
 fi
 if [ ! -d ${VIM_DIR}/rc ]; then
-  ln -s $HOME/dotf/rc ${VIM_DIR}/rc
+  ln -s $DOTFILEPATH/rc ${VIM_DIR}/rc
 fi
-ln -is $HOME/dotf/colors ${VIM_DIR}/colors
-ln -is $HOME/dotf/nvimrc $HOME/.vimrc
+ln -is $DOTFILEPATH/colors ${VIM_DIR}/colors
+ln -is $DOTFILEPATH/nvimrc $HOME/.vimrc
+ln -is $DOTFILEPATH/bin $HOME/bin
 
 # ln -is $HOME/dotf/ftplugin ${NVIM_DIR}/after/ftplugin
 # ln -is $HOME/dotf/ftplugin ${VIM_DIR}/after/ftplugin
