4a5840150877bff6a98831977ad7a61ce5473237
uedakn5 <ueda.kengo@accelight.co.jp>
2024-01-23 01:40:48 +0900

    modify init.lua

--- init.lua
+++ init.lua
@@ -491,7 +491,18 @@ neovim_plugins = {
 					-- { name = 'ultisnips' }, -- For ultisnips users.
 					-- { name = 'snippy' }, -- For snippy users.
 				}, {
-					{ name = 'buffer' },
+					{
+						name = 'buffer',
+						option = {
+							get_bufnrs = function()
+								local bufs = {}
+								for _, win in ipairs(vim.api.nvim_list_wins()) do
+									bufs[vim.api.nvim_win_get_buf(win)] = true
+								end
+								return vim.tbl_keys(bufs)
+							end
+						},
+					},
 				})
 			})
 
